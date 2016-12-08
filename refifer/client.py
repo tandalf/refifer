4e27443194a2e520c8c323585b59ccd4922094ae
Timothy <t.ebiuwhe@fetchr.us>
2016-12-08 10:02:27 +0100

    added some code to http client

--- refifer/client.py
+++ refifer/client.py
@@ -1,35 +1,46 @@
 """
 Module used for connecting with the ffire server
 """
-from requests import session, Request
+import requests 
 
 from .constants import RETRY_COUNT
 
 class Refifer(object):
-	"""
-	Class used for making api calls to the ffire server for registering
-	and pushing events.
-	"""
+    """
+    Class used for making api calls to the ffire server for registering
+    and pushing events.
+    """
 
-	def __init__(self, username=None, password=None, access_token=none, 
-		retry_count=RETRY_COUNT, timeout=None, proxies=none):
-	
-		self.username = username
-		self.password = password
-		self.timeout = timeout
-		self.proxies = proxies
-		self.retry_count = retry_count
+    def __init__(self, access_token=None, retry_count=RETRY_COUNT, 
+        timeout=None, proxies=None):
 
-	def register_event(self, event_name, endpoints):
-		"""
-		Registers an event with the server and provides a list of endpoints
-		that are to broadcasted to when the event is fired.
-		"""
-		raise NotImplemetedError("method not yet implemented")
+        self.access_token = access_token
+        self.timeout = timeout
+        self.proxies = proxies
+        self.retry_count = retry_count
+        self.session = requests.Session()
 
-	def publish_event(self, event_name, payload):
-		"""
-		Publishes an event to the server providing the payload that should
-		be broadcasted to the registed endpoints for the event.
-		"""
-		raise NotImplemetedError("method not yet implemented")
+    def _prepare_headers(self):
+        return {"access_token": self.access_token}
+
+    def request(self, endpoint, args=None, post_args=None, method=None):
+        """
+        Makes a request to the notifications api and returns the response.
+        if post_args are available, it makes a post request to the server.
+        """
+        if post_args:
+            method = "POST"
+
+    def register_event(self, event_name, endpoints):
+        """
+        Registers an event with the server and provides a list of endpoints
+        that are to broadcasted to when the event is fired.
+        """
+        raise NotImplemetedError("method not yet implemented")
+
+    def publish_event(self, event_name, payload):
+        """
+        Publishes an event to the server providing the payload that should
+        be broadcasted to the registed endpoints for the event.
+        """
+        raise NotImplemetedError("method not yet implemented")
