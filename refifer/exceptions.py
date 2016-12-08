71ed1c5c0fcfa8228a5a3c8e1467f2b669468462
Timothy <t.ebiuwhe@fetchr.us>
2016-12-08 10:03:08 +0100

    added some exceptions

--- refifer/exceptions.py
+++ refifer/exceptions.py
@@ -4,17 +4,28 @@ certain problems occur in the library.
 """
 
 class RefiferError(Exception):
-	"""
-	Base exception class which all Exceptions in this library should 
-	inherit from.
-	"""
-	pass
+    """
+    Base exception class which all Exceptions in this library should 
+    inherit from.
+    """
+    pass
 
 class AuthorizationError(RefiferError):
+    """
+    Exception raised when the client could not be authenticated on the
+    server.
+    """
+    pass
+
+class DuplicateEventError(RefiferError):
+    """
+    Raised when a duplicate event has been disallowed by the server.
+    """
+    pass
+
+class ValidationError(RefiferError):
 	"""
-	Exception raised when the client could not be authenticated on the
-	server.
+	Exception raised when the payload of an event fails to pass some
+	validation
 	"""
 	pass
-
-class
