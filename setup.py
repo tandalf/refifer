53f63218b4a450f7194fc5abd96ac8ef5f52ebbe
Timothy <t.ebiuwhe@fetchr.us>
2016-12-20 12:42:52 +0100

    updated package to support installation

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'refifer',
+  packages = ['refifer'], 
+  version = '0.1',
+  description = 'A client library used for registering and publishing' +\
+      ' events on fetchr\'s notification service',
+  author = 'Timothy Ebiuwhe',
+  author_email = 't.ebiuwhe@fetchr.us',
+  url = 'https://bitbucket.org/Fetchr/refifer', 
+  download_url = 'https://bitbucket.org/Fetchr/refifer/get/e79efae9fcb6.zip', # I'll explain this in a second
+  keywords = ['notifications', 'refifer', 'fetchr', 'fire'],
+  install_requires = ['requests', 'retrying']
+)
